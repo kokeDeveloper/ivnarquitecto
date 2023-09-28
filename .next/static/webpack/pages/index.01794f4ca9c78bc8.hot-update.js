157e7051634e844fdf46e0a34357ce32e8fdece5
Jorge Toledo <contacto@jorgetoledo.cl>
2023-09-28 16:47:56 -0300

    Primer push

---
+++ .next/static/webpack/pages/index.01794f4ca9c78bc8.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/Intro2.json":
+/*!******************************!*\
+  !*** ./src/data/Intro2.json ***!
+  \******************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('[{"id":1,"title":{"first":"soluciones de","second":"arquitectura"},"content":{"first":"30 años de experiencia en proyectos residenciales y comerciales.","second":"Diseños innovadores, funcionales y sostenibles que se adaptan a las necesidades y gustos de cada cliente."},"image":"/assets/img/slid/n/01.jpg"},{"id":2,"title":{"first":"innovación","second":"Diseño Interior"},"content":{"first":"Right design and right ideas matter a lot of in interior design","second":"business. a style that makes a statement."},"image":"/assets/img/slid/n/02.jpg"},{"id":3,"title":{"first":"Elegant &","second":"Diseños Únicos"},"content":{"first":"Right design and right ideas matter a lot of in interior design","second":"business. a style that makes a statement."},"image":"/assets/img/slid/n/03.jpg"}]');
+
+/***/ })
+
+});
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,91 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Link from "next/link";
+import appData from "../../data/app.json";
+import getSiblings from '../../common/getSiblings'
+
+const Navbar = ({ navbarRef, logoRef, logoClass }) => {
+  const handleDropdown = (e) => {
+    getSiblings(e.target.parentElement)
+      .filter((item) => item.classList.contains("show"))
+      .map((item) => {
+        item.classList.remove("show");
+        if (item.childNodes[0]) {
+          item.childNodes[0].setAttribute("aria-expanded", false);
+        }
+        if (item.childNodes[1]) {
+          item.childNodes[1].classList.remove("show");
+        }
+      });
+    e.target.parentElement.classList.toggle("show");
+    e.target.setAttribute("aria-expanded", true);
+    e.target.parentElement.childNodes[1].classList.toggle("show");
+  };
+
+  const handleMobileDropdown = (e) => {
+    document
+      .getElementById("navbarSupportedContent")
+      .classList.toggle("show-with-trans");
+  };
+
+  return (
+    <>
+      <nav className="navbar change navbar-expand-lg" ref={navbarRef}>
+        <div className="container">
+          <Link href="/demos">
+            <a className={`logo ${logoClass && logoClass}`}>
+              <img src={appData.lightLogo} alt="logo" ref={logoRef} />
+            </a>
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            onClick={handleMobileDropdown}
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="icon-bar">
+              <i className="fas fa-bars"></i>
+            </span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <Link href="/about">
+                  <a className="nav-link">Home</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/about">
+                  <a className="nav-link">¿Quién soy?</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/about">
+                  <a className="nav-link">Portafolio</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/blog">
+                  <a className="nav-link">Blog</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/contact">
+                  <a className="nav-link">Contacto</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
